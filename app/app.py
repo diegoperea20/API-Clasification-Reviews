cd11bdb8471386cd2856e2cef85969f303a28b2f
diegoperea20 <diegoperea20@gmail.com>
2023-05-25 10:24:23 -0500

    fixed name and deployment

--- app/app.py
+++ app/app.py
@@ -50,5 +50,5 @@ def clasification():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run(debug=True,host='0.0.0.0')
+    #app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
